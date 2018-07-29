0face7257409718d2c0723b1fee4c8335faca9aa
Sebastian Muñoz <smunoz@celmedia.cl>
2018-07-28 21:11:33 -0400

    3 (__(___((((() already

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 // API calls
+
+var redis = require("redis").createClient();
+
+if (process.env.REDISTOGO_URL) {
+	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+	var redis = require("redis").createClient(rtg.port, rtg.hostname);
+
+	redis.auth(rtg.auth.split(":")[1]);
+} else {
+    var redis = require("redis").createClient();
+}
+
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
